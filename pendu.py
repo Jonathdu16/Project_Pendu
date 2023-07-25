8bf60dc85c5f9bdc15222f3a905e42d59b1f9a15
Jonathan COURTOIS <Jonathan.courtois.pro@gmail.com>
2023-07-25 13:29:26 +0200

    Menu finaliser

--- pendu.py
+++ pendu.py
@@ -7,26 +7,34 @@ fichierXML = "C:/xampp/htdocs/Python/Projets/Pendu/Project_Pendu/menu.xml"
 window = Tk()
 window.geometry('600x600')
 window.resizable(0, 0)
+window.title('Jeu du Pendu')
+
+# def 
 
 # Chargement du fichier XML
 structureXML = ET.parse(fichierXML)
-nodeElement = minidom.parse(fichierXML)
-belowEnsemble = nodeElement.getElementsByTagName('label')
 ensemble = structureXML.getroot()
 
-print(ensemble[1].attrib["categorie"])
-
 navbar = Menu(window, background="#d9d9d9")
-navbar_list = Menu(navbar, tearoff=0)
 
+low_menu_by_category = {}
     
-for element in ensemble:
-    navbar.add_cascade(label = element.attrib['categorie'], menu = navbar_list)
+for lowElement in ensemble.findall('menu'):
+    categorie = lowElement.attrib["categorie"]
 
-for belowElement in belowEnsemble:
-    belowNavbar = navbar_list.add_command(label = belowElement.firstChild.nodeValue)
+    if categorie not in low_menu_by_category:
+        low_menu_by_category[categorie] = Menu(navbar, tearoff=0)
 
+    navbar_list = low_menu_by_category[categorie]
 
+    for belowElement in lowElement.findall('lien'):
+        label_element = belowElement.find('label')
+        if label_element is not None:
+            navbar_list.add_command(label = label_element.text)
+
+for categorie, sous_menu in low_menu_by_category.items():
+
+    navbar.add_cascade(label=categorie, menu=sous_menu)
 
 window.config(menu = navbar)
 
